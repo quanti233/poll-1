ece4508dc717d220b72fbbd44a4dbd91d44871cc
11330147 <11330147@dcsh.tp.edu.tw>
2025-12-05 11:45:49 +0800

    完成投票主題列表查看投票主題和功能

--- default/models.py
+++ default/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class poll(models.Model):
+    subject = models.CharField("投票主題", max_length=64)
+    desc = models.TextField("說明")
+    created = models.DateField("建立日期",auto_now_add=True)
+
+    def __str__(self):
+        return self.subject
+
+class Option(models.Model):
+    title = models.CharField("選項文字", max_length=64)
+    votes = models.IntegerField("票數", default=0)
+    poll_id = models.IntegerField("投票主題編號")
+
+    def __str__(self):
+        return f"{self.poll_id} - {self.title} : {self.votes}"
